aa8561a60240d3489437684b6864bf5b727e65a9
Paul Lorber <paul.lorber@protonmail.com>
2025-01-14 16:43:50 +0100

    simplify RootLayout component by reducing type declaration verbosity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,7 @@
 
 import "./globals.css";
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
         {children}
